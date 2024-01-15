229035f72b4849d2e392e3303b5be122a2238636
Ina Yoon <yia0319@gmail.com>
2024-01-15 23:59:13 +0900

    Add calendar using react-calendar

--- frontend/src/components/AddTransaction.jsx
+++ frontend/src/components/AddTransaction.jsx
@@ -1,6 +1,18 @@
-import React from "react";
-import { BsPlusCircleDotted } from "react-icons/bs";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 export default function AddTransaction() {
-  return <div>sdfsdf</div>;
+  const [value, onChange] = useState(new Date());
+
+  return (
+    <section>
+      <div className="mt-6">
+        New Transaction
+        <div className="w-[90%] my-[1%] border-[1px] border-lightGray/30"></div>
+      </div>
+      <div className="font-semibold">Date</div>
+      <Calendar onChange={onChange} value={value} locale="en-US" />
+    </section>
+  );
 }
